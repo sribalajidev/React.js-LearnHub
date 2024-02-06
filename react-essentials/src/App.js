44cc6b897e89a1b9b57d981c6c320cd2968d672e
Sri Balaji <developer.sribalaji@gmail.com>
2024-02-06 15:52:17 +0530

    LH-1 / Restructering Components

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -2,25 +2,16 @@ import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
 import HiddenTab from "./components/HiddenTab/HiddenTab";
 
-import { CORE_CONCEPTS } from './data.js';
-
 function App() {
   return (
-    <div>
+    <>
+    <Header />
       <main>
-        <Header />
         <h2>Time to get started!</h2>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcepts key={conceptItem.title} {...conceptItem}/>
-            ))}
-          </ul>
-        </section>
+        <CoreConcepts />
         <HiddenTab />
       </main>
-    </div>
+    </>
   );
 }
 
--- react-essentials/src/components/CoreConcepts/CoreConcepts.js
+++ react-essentials/src/components/CoreConcepts/CoreConcepts.js
@@ -1,12 +1,25 @@
-import "./CoreConcepts.scss"
+import "./CoreConcepts.scss";
+import { CORE_CONCEPTS } from '../../data';
 
-function CoreConcepts({image, title, description}) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
     </li>
+  );
+}
+function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </section>
   );
 }
 
